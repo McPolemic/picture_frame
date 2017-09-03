36d40ec2f4b0399f9bd92cbc1b846c730f13cc94
Adam Lukens <adam@testdouble.com>
2017-09-03 02:03:55 -0400

    Add a TODO

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker build -t picture_frame .
 docker run --name=picture_frame                 \
 	   --restart=always                     \
 	   -e "REFRESH_IN_MS=30000"             \
+	   -e "MINIMUM_SIZE_IN_BYTES=30000"     \
 	   -d                                   \
 	   -p 4000:4000                         \
 	   -v /media/storage/Pictures:/pictures \
