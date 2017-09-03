a08ca8cca8b715ad35a7cf041bdedd95ffec148e
Adam Lukens <adam@testdouble.com>
2017-09-03 01:14:43 -0400

    Make the thing

---
+++ app.rb
@@ -0,0 +1,71 @@
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+set :port, 4000
+
+def all_pictures(path)
+  suffixes = FORMATS + FORMATS.map(&:upcase)
+
+  suffixes.flat_map do |suffix|
+    search_string = File.join(path, "**/*.#{suffix}")
+    Dir.glob(search_string)
+  end.reject{ |path| File.size(path) < 1_000 }
+end
+
+FORMATS = %w(jpg jpeg gif png)
+PICTURES_PATH = ENV.fetch("PICTURES_PATH")
+PICTURES = all_pictures(PICTURES_PATH)
+REFRESH_IN_MS = ENV.fetch("REFRESH_IN_MS", 3_000)
+
+get '/' do
+  erb :index
+end
+
+get '/random' do
+  picture_path = PICTURES.sample
+  puts "Serving #{picture_path}"
+
+  send_file(picture_path)
+end
+
+get '/public/*' do
+  picture_path = File.join(PICTURES_PATH, params[:splat].first)
+  send_file(picture_path)
+end
+
+__END__
+
+@@ index
+<html>
+  <head>
+    <title>Picture Frame</title>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <style>
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+body {
+  background-color: black;
+}
+    </style>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+            crossorigin="anonymous"></script>
+    <script>
+$(document).ready(function() {
+  setInterval(function() {
+    $('img').prop("src", "/random?dummy=" + Date.now());
+  }, <%= REFRESH_IN_MS %>);
+});
+    </script>
+  </head>
+  <body>
+  <img src="/random">
+  </body>
+</html>
+
